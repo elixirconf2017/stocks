0b4feced1d32b3997d5ba1d7dcbf66f415373fe1
Andrew Forward <aforward@gmail.com>
2017-08-16 17:17:40 -0400

    Support monthly

--- lib/stocks.ex
+++ lib/stocks.ex
@@ -1,5 +1,11 @@
 defmodule Stocks do
+
   def yield(stock_price, payout, :quarterly) do
     payout * 4 / stock_price
   end
+
+  def yield(stock_price, payout, :monthly) do
+    payout * 12 / stock_price |> Float.round(3)
+  end
+
 end
--- test/stocks_test.exs
+++ test/stocks_test.exs
@@ -2,7 +2,11 @@ defmodule StocksTest do
   use ExUnit.Case
   # doctest Stocks
 
-  test "yield" do
+  test "yield (quarterly)" do
     assert 0.02 == Stocks.yield(10, 0.05, :quarterly)
+  end
+
+  test "yield (monthly)" do
+    assert 0.12 == Stocks.yield(10, 0.1, :monthly)
   end
 end
