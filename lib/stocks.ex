47536ed98ac413b3a60ee2e4c4539b40fe120458
Andrew Forward <aforward@gmail.com>
2017-08-17 11:57:01 -0400

    Refactor yield into consolidated num_per_year calc

--- lib/stocks.ex
+++ lib/stocks.ex
@@ -1,11 +1,15 @@
 defmodule Stocks do
 
   def yield(stock_price, payout, :quarterly) do
-    payout * 4 / stock_price
+    yield(stock_price, payout, 4)
   end
 
   def yield(stock_price, payout, :monthly) do
-    payout * 12 / stock_price |> Float.round(3)
+    yield(stock_price, payout, 12)
+  end
+
+  def yield(stock_price, payout, num_per_year) when is_integer(num_per_year) do
+    payout * num_per_year / stock_price |> Float.round(3)
   end
 
 end
